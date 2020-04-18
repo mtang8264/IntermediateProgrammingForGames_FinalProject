868d06a618c84fdbc591c072ec5d1ba4c71c8599
Michael Tang <mtang8264@gmail.com>
2020-04-18 15:01:39 -0400

    Created a simple heart
    The heart can beat and slowly die but there are no consequence yet

---
+++ Assets/Scripts/Heart.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heart : MonoBehaviour
+{
+    KeyCode heartbeatKey = KeyCode.H;
+    float timer;
+    Animator animator;
+    [SerializeField]
+    float minTime, maxTime;
+    [SerializeField]
+    HeartBar bar;
+
+    private void Awake() {
+        animator = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer < minTime)
+            bar.SetQuality(HeartBar.Timing.EARLY);
+        else if (timer > maxTime)
+            bar.SetQuality(HeartBar.Timing.LATE);
+        else
+            bar.SetQuality(HeartBar.Timing.GOOD);
+
+        animator.SetBool("Asphyxiating", timer > maxTime);
+
+        if(Input.GetKeyDown(heartbeatKey))
+        {
+            Heartbeat();
+        }
+
+        bar.SetPosition(timer/maxTime);
+    }
+
+    void Heartbeat()
+    {
+        animator.SetTrigger("Beat");
+        timer = 0f;
+    }
+}
---
+++ Assets/Scripts/HeartBar.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartBar : MonoBehaviour
+{
+    [SerializeField]
+    Transform indicator;
+    float position;
+    [SerializeField]
+    Color earlyIndicator, goodIndicator, lateIndicator;
+    public enum Timing {EARLY, GOOD, LATE};
+    Timing timing;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float xPos = Mathf.Lerp(-0.5f, 0.5f, position);
+        Vector3 newPos = new Vector3(xPos,indicator.localPosition.y, indicator.localPosition.z);
+        indicator.localPosition = newPos;
+
+        switch(timing)
+        {
+            case Timing.EARLY:
+                indicator.GetComponent<SpriteRenderer>().color = earlyIndicator;
+                break;
+            case Timing.GOOD:
+                indicator.GetComponent<SpriteRenderer>().color = goodIndicator;
+                break;
+            case Timing.LATE:
+                indicator.GetComponent<SpriteRenderer>().color = lateIndicator;
+                break;
+        }
+    }
+
+    public void SetPosition(float percentage)
+    {
+        position = percentage;
+    }
+
+    public void SetQuality(Timing t)
+    {
+        timing = t;
+    }
+}
