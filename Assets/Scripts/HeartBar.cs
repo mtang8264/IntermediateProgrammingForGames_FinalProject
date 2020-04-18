c3d01e639377f4d2fb6b72bed03b8e1f0f72f405
Michael Tang <mtang8264@gmail.com>
2020-04-18 15:07:49 -0400

    Added additional comments

--- Assets/Scripts/HeartBar.cs
+++ Assets/Scripts/HeartBar.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class HeartBar : MonoBehaviour
 {
     [SerializeField]
-    Transform indicator;
-    float position;
+    Transform indicator;    // The circle indicator of when to push the button
+    float position; // The position from left to right as a float [0,1]
     [SerializeField]
-    Color earlyIndicator, goodIndicator, lateIndicator;
+    Color earlyIndicator, goodIndicator, lateIndicator; // These are the colors the indicator turns based on when the player should push the button
     public enum Timing {EARLY, GOOD, LATE};
     Timing timing;
 
@@ -19,10 +19,12 @@ public class HeartBar : MonoBehaviour
 
     void Update()
     {
+        // This finds the local x position the indicator should be at
         float xPos = Mathf.Lerp(-0.5f, 0.5f, position);
         Vector3 newPos = new Vector3(xPos,indicator.localPosition.y, indicator.localPosition.z);
         indicator.localPosition = newPos;
 
+        // Based on when in the timing process it is we change the color of the indicator
         switch(timing)
         {
             case Timing.EARLY:
@@ -37,11 +39,13 @@ public class HeartBar : MonoBehaviour
         }
     }
 
+    // This updates where the indicator should be given a percentage from left to right
     public void SetPosition(float percentage)
     {
         position = percentage;
     }
 
+    // This updates if the heartbeat will be early, late or good
     public void SetQuality(Timing t)
     {
         timing = t;
